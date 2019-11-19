c765efae853c70bc4684e0e16603479e75ec5874
Ariane <ariane@pop-os.localdomain>
2019-11-18 21:47:05 -0300

    add requirements.txt

--- instag/core/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import User, Foto
-
-admin.site.register(User)
-admin.site.register(Foto)
--- instag/core/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import *
-
-class LoginForm(forms.Form):
-
-    username = forms.CharField(widget=forms.TextInput())
-    password = forms.CharField(widget=forms.PasswordInput())
